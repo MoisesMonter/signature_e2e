9ac6824a70dbf7f498f0c23d642f5ec4bfc41926
MoisesMonteiro <moises.monteiro2077@hotmail.com>
2025-02-26 13:17:22 -0300

    Update user_page

--- cypress/e2e/Pagina_Usuario.cy.js
+++ cypress/e2e/Pagina_Usuario.cy.js
@@ -1,6 +1,32 @@
 
 describe('Testes de Gerenciamento', () => {
-    beforeEach(() => {
-    })
-  
+  beforeEach(() => {
+    cy.clearCookies()
+    cy.clearLocalStorage()
+    cy.login()
   })
+
+  
+  it('Encontrnado a rota Usuario', () => {
+    cy.wait(1000)
+    cy.get("#User").click()
+  })
+
+
+  it('Testando Campos nova assinatura', () => {
+      cy.wait(1000)
+      cy.get("#User").click()
+      cy.wait(1000)
+      cy.contains("Assinar").click()
+      cy.wait(1000)
+      cy.contains("Fechar").click()
+      cy.contains("Assinar").click()
+      cy.wait(4000)
+      cy.contains("Limpar").click()
+      cy.wait(4000)
+      cy.contains("Salvar").click()
+      cy.contains("Sucesso").should('exist')
+  })
+
+
+})
