1214742c7167539385249bb9e0a4b808c74d69e5
MoisesMonteiro <moises.monteiro2077@hotmail.com>
2025-02-26 13:16:20 -0300

    Update commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,3 +24,30 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
+Cypress.Commands.add('login', () => {
+  cy.viewport(1600, 900)
+  cy.visit('http://localhost:8081');
+  cy.getCookie("auth_jrf").should("be.null");
+  cy.get('#logout').click();
+  cy.contains("Google").click();
+  cy.visit('http://localhost:8081');
+})
+
+
+Cypress.Commands.add('dragAndDrop', (sourceSelector, targetSelector) => {
+  cy.get(sourceSelector).then(($source) => {
+    const sourceRect = $source[0].getBoundingClientRect();
+    const sourceX = sourceRect.left + (sourceRect.width / 2);
+    const sourceY = sourceRect.top + (sourceRect.height / 2);
+    cy.get(sourceSelector).trigger('mousedown', { which: 1, pageX: sourceX, pageY: sourceY });
+
+    cy.get(targetSelector).then(($target) => {
+      const targetRect = $target[0].getBoundingClientRect();
+      const targetX = targetRect.left + (targetRect.width / 2);
+      const targetY = targetRect.top + (targetRect.height / 2);
+      cy.get('body').trigger('mousemove', { pageX: targetX, pageY: targetY });
+    });
+
+    cy.get(targetSelector).trigger('mouseup');
+  });
+});
