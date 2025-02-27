b9ac0009a4bda973634e8b30ee80bdef4f6a9c9f
MoisesMonteiro <moises.monteiro2077@hotmail.com>
2025-02-27 17:40:32 -0300

    Update test

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 require('dotenv').config();
+
 module.exports = defineConfig({
+  viewportWidth: 1600,       // Largura padrão da tela
+  viewportHeight: 900,       // Altura padrão da tela
+  defaultCommandTimeout: 10000, // Tempo de espera padrão de 10 segundos
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
